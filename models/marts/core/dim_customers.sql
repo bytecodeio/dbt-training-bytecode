c1aad83d63c16590dba1ceeb894554ff364872f0
Eugene Aiken <eugene.aiken@bytecode.io>
2022-01-11 21:59:36 +0000

    Second Commit

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,6 +1,11 @@
 with customers as (
     select * from {{ ref('stg_customers')}}
 ),
+
+employees as (
+    select * from {{ ref('employees')}}
+),
+
 orders as (
     select * from {{ ref('fct_orders')}}
 ),
@@ -19,11 +24,13 @@ final as (
         customers.customer_id,
         customers.first_name,
         customers.last_name,
+        employees.employee_id is not null as is_employee,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
         customer_orders.lifetime_value
     from customers
     left join customer_orders using (customer_id)
+    left join employees using (customer_id)
 )
 select * from final
